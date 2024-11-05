93e4bb4d5441cc79f8b05eb23121707f9b805099
victor-ok <vua.okafor@gmail.com>
2024-11-05 12:53:08 +0100

    storage module added

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# gke-iac
+
+# A simple Python application deployed to GKE
+
+
+
+## Project Requirements
+
+Terraform
+
+GCP Account
+
+Kubernetes
+
+Kubectl
+
+
+
+
+## Deliverables
+
+- Deployed using an Infrastructure as Code approach(Terraform).
+- Application deployment should be clear, readable and maintainable.
+- You can use an IaaS provider of your choice(I will be using GCP).
+- The application should run on Kubernetes
+
+
+## Provison Infrastructure
+
+With Terraform, we will provision the necessary infrastructure resources on Azure[resource group, vn, subnets and eks].
+
+* Confirm you have gcp cli and Terraform installed with the commands:
+```bash
+  gcloud --version
+
+  terrform --version
+
+  gcloud init --console-only --account [your gcp account details] --project[gcp project in your account]
+
+```
+
+* Git clone this repository and navigate to the infrastructure folder.
+```bash
+    https://github.com/victor-ok/sock-shop-deployment.git
+
+    cd infrastructure
+```
+
+* Run the following commands to initialize, plan and apply the main.tf
+```bash
+    terraform init
+
+    terraform plan
+    
+    terraform apply
+```
+
+* Your resources should be running after a short while.
--- infrastructure/module/storage/main.tf
+++ infrastructure/module/storage/main.tf
@@ -1,38 +1,38 @@
-# resource "random_id" "default" {
-#   byte_length = 8
-# }
+resource "random_id" "default" {
+  byte_length = 8
+}
 
-# resource "google_storage_bucket" "my_bucket" {
-#   name                        = "${random_id.default.hex}-backend-bucket"
-#   location                    = "US"
-#   force_destroy               = false
-#   public_access_prevention    = "enforced"
-#   uniform_bucket_level_access = true
-#   // Object Versioning must be enabled 
-#   versioning {
-#     enabled = true
-#   }
-# }
+resource "google_storage_bucket" "my_bucket" {
+  name                        = "${random_id.default.hex}-backend-bucket"
+  location                    = "US"
+  force_destroy               = false
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+  // Object Versioning must be enabled 
+  versioning {
+    enabled = true
+  }
+}
 
-# resource "google_storage_bucket_iam_member" "member" {
-#   bucket = google_storage_bucket.my_bucket.name
-#   role   = "roles/storage.admin"
-#   member = var.serviceAccount
-# }
+resource "google_storage_bucket_iam_member" "member" {
+  bucket = google_storage_bucket.my_bucket.name
+  role   = "roles/storage.admin"
+  member = var.serviceAccount
+}
 
-# resource "local_file" "default" {
-#   file_permission = "0644"
-#   # filename        = "${path.module}/backend.tf"
-#   filename = "${path.root}/provider.tf"
+resource "local_file" "default" {
+  file_permission = "0644"
+  # filename        = "${path.module}/backend.tf"
+  filename = "${path.root}/provider.tf"
 
-#   # You can store the template in a file and use the templatefile function for
-#   # more modularity, if you prefer, instead of storing the template inline as
-#   # we do here.
-#   content = <<-EOT
-#   terraform {
-#     backend "gcs" {
-#       bucket = "${google_storage_bucket.my_bucket.name}"
-#     }
-#   }
-#   EOT
-# }
+  # You can store the template in a file and use the templatefile function for
+  # more modularity, if you prefer, instead of storing the template inline as
+  # we do here.
+  content = <<-EOT
+  terraform {
+    backend "gcs" {
+      bucket = "${google_storage_bucket.my_bucket.name}"
+    }
+  }
+  EOT
+}
